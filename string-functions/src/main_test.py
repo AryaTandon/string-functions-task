712307e5f867760c8229dc816879d5437801e3cc
Arya Tandon <atandon23@outlook.com>
2021-01-28 12:00:50 +0000

    A second lot of tests

--- string-functions/src/main_test.py
+++ string-functions/src/main_test.py
@@ -2,3 +2,6 @@ import main
 
 def test_capitalise_string():
     assert main.capitalise_string("word") == "word".upper()
+    assert main.capitalise_string("one") == "one".upper()
+    assert main.capitalise_string("two") == "two".upper()
+    assert main.capitalise_string("three") == "three".upper()
