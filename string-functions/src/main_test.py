22044155bfc9988342633b30dc671d6b1b722d80
Arya Tandon <atandon23@outlook.com>
2021-01-28 12:22:10 +0000

    Separated out tests and updated main.py

--- string-functions/src/main_test.py
+++ string-functions/src/main_test.py
@@ -2,6 +2,12 @@ import main
 
 def test_capitalise_string():
     assert main.capitalise_string("word") == "word".upper()
+
+def test_capitalise_string2():
     assert main.capitalise_string("one") == "one".upper()
+
+def test_capitalise_string3():
     assert main.capitalise_string("two") == "two".upper()
+
+def test_capitalise_string4():
     assert main.capitalise_string("three") == "three".upper()
